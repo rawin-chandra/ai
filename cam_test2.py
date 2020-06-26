b95e960875431dde2009d8b7aba78fa50f4cea8f
rawin-chandra <40013619+rawin-chandra@users.noreply.github.com>
2020-06-26 19:46:58 +0700

    Update cam_test2.py

--- cam_test2.py
+++ cam_test2.py
@@ -1,30 +1,36 @@
+'''
+credit:
+https://www.geeksforgeeks.org/detect-an-object-with-opencv-python
+
+https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_objdetect/py_face_detection/py_face_detection.html
+
+create by T. Rawin (T. S)
+if you use webcam you should change VideoCapture(0) to VideoCapture(1)
+'''
+
+
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt 
 
-cap = cv2.VideoCapture(1)
-#stop_data = cv2.CascadeClassifier('stop_data.xml')
+cap = cv2.VideoCapture(0)
 face = cv2.CascadeClassifier('face.xml')
 
 while(True):
    
     ret, frame = cap.read()    
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-     
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)    
 
-    #found = stop_data.detectMultiScale(gray,minSize =(20, 20))   
+     
     found = face.detectMultiScale(gray,minSize =(20, 20)) 
 
     amount_found = len(found) 
 
-    if amount_found != 0: 
-	
+    if amount_found != 0: 	
 	
 	for (x, y, width, height) in found: 		
 		
-		cv2.rectangle(frame, (x, y), 
-					(x + height, y + width), 
-					(0, 255, 0), 5) 
+		cv2.rectangle(frame, (x, y),(x + height, y + width),(0, 255, 0), 5) 
 
    
     cv2.imshow('frame',frame) 
